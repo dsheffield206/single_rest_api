4058d5bc513f2f2de561858d78dc087e0d6e88d5
David Sheffield <david.sheffield21@gmail.com>
2015-09-14 22:47:11 -0700

    setting up team schema for REST API

--- models/team.js
+++ models/team.js
@@ -1,3 +1,13 @@
-'use strict';
+var mongoose = require('mongoose');
 
-// add schema for team db
+var teamSchema = new mongoose.Schema({
+    teamBody: String,
+    name: String,
+    position: String,
+    years: String,
+    high_school: String,
+    NFL: String,
+    NFLDraft: String
+});
+
+module.exports = mongoose.model('Player', teamSchema);
