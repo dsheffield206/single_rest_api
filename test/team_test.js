3fb737a180cc1d97ae3770c114c7624ce2bd8351
David Sheffield <david.sheffield21@gmail.com>
2015-09-14 23:00:37 -0700

    adding testing

--- test/team_test.js
+++ test/team_test.js
@@ -7,5 +7,19 @@ var expect = chai.expect;
 process.env.MONGO_URL = 'mongodb://localhost/team_test';
 var mongoose = require('mongoose');
 var url = 'localhost:3000/api';
+var Player = require(__dirname + '/../models/team');
 
-// describe & it yada yada
+describe('the team resource', function(){
+    after(function(done){
+        mongoose.connection.db.dropDatabase(function(err){
+            if(err) throw err;
+            done();
+        });
+    });
+
+    it('should be able to count collection', function(done){
+        chai.request(url)
+            .count('collection')
+
+    });
+});
