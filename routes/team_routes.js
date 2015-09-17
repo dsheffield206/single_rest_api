da27f5bceb403e856d8d2e45cee8e89d7b8355e9
David Sheffield <david.sheffield21@gmail.com>
2015-09-17 14:25:51 -0700

    updating REST API for tests

--- routes/team_routes.js
+++ routes/team_routes.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Player = require(__dirname + '/../models/team');
+var lsutigers = require(__dirname + '/../models/team');
 var express = require('express');
 var jsonParser = require('body-parser').json();
 var handleError = require(__dirname + '/../lib/handle_error');
@@ -8,21 +8,48 @@ var handleError = require(__dirname + '/../lib/handle_error');
 var teamRoute = module.exports = exports = express.Router();
 
 teamRoute.get('/team', function(req, res){
-    Player.find({}, function(err, data){
+    lsutigers.find({}, function(err, data){
         if(err) return handleError(err, res);
         res.json(data);
     });
 });
 
 teamRoute.post('/team', jsonParser, function(req, res){
-    var newPlayer = new Player(req.body);
-    newPlayer.save(function(err, data){
+    var newlsutigers = new lsutigers(req.body);
+    newlsutigers.save(function(err, data){
         if(err) return handleError(err, res);
         res.json(data);
     });
 });
 
+teamRoute.put('/team/:id', jsonParser, function(req, res){
+    var newlsutigersBody = req.body;
+    delete newlsutigersBody._id;
+    lsutigers.update({_id: req.params.id}, newlsutigersBody, function(err, data){
+        if(err) return handleError(err, res);
+        res.json({msg: 'your update was a success'});
+    });
+});
+
+teamRoute.delete('/team/:id', jsonParser, function(req, res){
+    lsutigers.remove({_id: req.params.id}, function(err){
+        if(err) return handleError(err, res);
+        res.json({msg: 'your delete went through'});
+    });
+});
+
 
 // NonCRUD Stuff
-teamRoute.db.collection.count();
-teamRoute.db.collection.sort();
+teamRoute.count('/team', function(req, res){
+    lsutigers.count({}, function(err, data){
+        if(err) return handleError(err, res);
+        res.json({msg: 'success counting players!'});
+    });
+});
+
+teamRoute.sort('/team', function(req, res){
+    lsutigers.sort({}, function(err, data){
+        if(err) return handleError(err, res);
+        res.json({msg: 'you just sorted the team, yo!'});
+    });
+});
