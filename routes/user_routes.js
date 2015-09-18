a8d3912ce418738a1129f5bf4823c4239df9090c
David Sheffield <david.sheffield21@gmail.com>
2015-09-18 15:08:57 -0700

    adding event emitters to user_routes for auth

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -3,6 +3,9 @@ var User = require(__dirname + '/../models/user');
 var jsonParser = require('body-parser').json();
 var httpBasic = require(__dirname + '/../lib/http_basic');
 var handleError = require(__dirname + '/../lib/handle_error');
+var EventEmitter = require('events').EventEmitter;
+var ee = new EventEmitter();
+var user;
 
 var app = module.exports = exports = express.Router();
 
@@ -13,25 +16,35 @@ app.get('/', function(req, res, next){
 });
 
 app.get('/signin', httpBasic, function(req, res){
+    ee.emit('findOne', req, res, user);
+});
+
+ee.on('findOne', function(req, res, user){
     User.findOne({'basic.username': req.auth.username}, function(err, user){
         if(err) return handleError(err, res);
         if(!User){
             console.log('could not authenticate ' + req.auth.username);
             return res.status(401).json({success: false, msg: 'could not authenticate'});
         }
+        ee.emit('compareHash', req, res, user);
+    });
+});
 
-        User.compareHash(req.auth.password, function(err, hashRes){
-            if(err) return handleError(err, res);
-            if(!hashRes){
-                console.log('could not authenticate ' + req.auth.username);
-                return res.status(401).json({success: false, msg: 'could not authenticate'});
-            }
+ee.on('compareHash', function(req, res, user){
+    User.compareHash(req.auth.password, function(err, hashRes){
+        if(err) return handleError(err, res);
+        if(!hashRes){
+            console.log('could not authenticate ' + req.auth.username);
+            return res.status(401).json({success: false, msg: 'could not authenticate'});
+        }
+        ee.emit('generateToken', req, res, user);
+    });
+});
 
-            User.generateToken(function(err, token){
-                if(err) return handleError(err, res);
-                res.json({success: true, token: token});
-            });
-        });
+ee.on('generateToken', function(req, res, user){
+    User.generateToken(function(err, token){
+        if(err) return handleError(err, res);
+        res.json({success: true, token: token});
     });
 });
 
@@ -39,14 +52,25 @@ app.post('/signup', jsonParser, function(req, res){
     var newUser = new User();
     newUser.basic.username = req.body.username;
     newUser.username = req.body.username;
+    ee.emit('generateHash', newUser, req, res);
+});
+
+ee.on('generateHash', function(newUser, req, res){
     newUser.generateHash(req.body.password, function(err, hash){
         if(err) return handleError(err, data);
-        newUser.save(function(err, data){
-            if(err) return handleError(err, data);
-            newUser.generateToken(function(err, token){
-                if(err) return handleError(err, res);
-                res.json({success: true, token: token});
-            });
-        });
+        ee.emit('save', newUser, req, res);
     });
+});
+
+ee.on('save', function(newUser, req, res){
+    newUser.save(function(err, data){
+        if(err) return handleError(err, data);
+        ee.emit('generateToken', newUser, req, res);
+    });
+});
+
+ee.on('generateNewToken', function(newUser, req, res){
+    newUser.generateToken(function(err, token){
+        if(err) return handleError(err, res);
+        res.json({success: true, token: token});
 });
