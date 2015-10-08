390b246b76c2d7655bfaac627e1ba031302ce615
David Sheffield <david.sheffield21@gmail.com>
2015-10-08 10:43:46 -0700

    adding the directive to the html

--- app/js/client.js
+++ app/js/client.js
@@ -1,5 +1,8 @@
 require('angular/angular');
+var angular = window.angular;
 
 var teamApp = angular.module('teamApp', []);
+
+require('./services/services')(teamApp);
 require('./team/team')(teamApp);
 
