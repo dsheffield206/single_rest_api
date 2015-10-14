ec0d2be2e23bec88a4e07d17fd5ea4e704dffd7d
David Sheffield <david.sheffield21@gmail.com>
2015-10-14 12:18:17 -0700

    updating a type-o in router.js

--- app/js/router.js
+++ app/js/router.js
@@ -13,7 +13,7 @@ module.exports = function(teamApp){
                 controller: 'SigninController'
             })
             .otherwise({
-                redirectTo: 'signup'
+                redirectTo: '/signup'
             });
     }]);
 };
